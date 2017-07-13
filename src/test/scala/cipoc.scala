e34c4ccdff5ab6f7df4bf9b4dcf4327d647fac95
hrishi0009 <hrishikesh.kshatriya@antuit.com>
2017-07-13 13:20:55 +0530

    code testing

---
+++ src/test/scala/cipoc.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by hrishikesh.kshatriya on 7/13/2017.
+  */
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
+import java.security.MessageDigest
+import org.apache.spark.sql.Dataset
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
+import scala.reflect.runtime.universe
+import java.util.Calendar
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.functions.lit
+
+object cipoc {
+  def main(args: Array[String]): Unit = {
+
+
+    val conf = new SparkConf().setAppName("DeltaAdd     "+table)
+    val sc = new SparkContext(conf)
+    val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
+
+
+    val df = hiveContext.read.format("com.databricks.spark.csv").option("header", "true").load("/home/gitrepo/CSV_Files/Real_Estate1.xlsx")
+
+    df.registerTempTable("csvload")
+
+    hiveContext.sql("insert into real_estate_trans select * from csvload")
+
+  }
+
+}
